8526b4b3ee91784f830e11eac4b996bcc354d4c0
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-01-29 18:09:56 +0500

    feat"

--- JavascriptBased/TwoSum.js
+++ JavascriptBased/TwoSum.js
@@ -15,7 +15,7 @@
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
- 
+
 
 // Constraints:
 
@@ -30,22 +30,43 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     //1) See Till here Just I have outputted the values that I passed in the function just to verify
     // console.log(nums)
     // console.log(target)
 
     //2) Step two I will loop through the array thats it
-    for(let i=0;i<nums.length;i++)
-    {
+    for (let i = 0; i < nums.length; i++) {
         //Nothing fancy just printing the values of the array thats how I am approaching
         console.log(nums[i]);
     }
 
+    //3) Step three I will make a variable that will store the result when looping through the array
+    var sum = 0;
+    //This will store the final result
+    var newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+        //Nothing fancy just printing the values of the array thats how I am approaching
+        
+        for (let j = i++; nums.length; j++) {
+            sum = nums[i] + nums[j];
+            if (sum == target) {
+                newArr.push(i);
+                newArr.push(j)
+                return newArr;
+            }
+        }
+
+        console.log(nums[i]);
+    }
+
+    console.log("The final array is : ", newArr)
+    //Thats it I got it and now lets return 
+    return newArr;
 };
 
 
-let nums = [2,7,11,15]
-let target = 9
+let nums = [3,2,4]
+let target = 6
 
-twoSum(nums,target)
+console.log("The answer is : ",twoSum(nums, target))
